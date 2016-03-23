4d5c23662712c23c7fbf87f3660acb0e8e9a99a4
huwei <2570328000@qq.com>
2016-03-23 19:59:54 +0800

    wap_indexdd

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,7 +3,7 @@
 		$config['dbport'] = '';  //数据库用户
 		$config['dbuser'] = 'root';  //数据库用户
 		$config['dbpass'] = 'root';   	 //数据库密码
-		$config['dbname'] = 'mallbuilder_bwj';     //数据库名
+		$config['dbname'] = 'mallbuilder';     //数据库名
 		$config['table_pre']='mallbuilder_';  //数据库表前缀
 		$config['authkey']='2c7832406405c2479e3da150925a73e0';  //数据库表前缀
 		?>
