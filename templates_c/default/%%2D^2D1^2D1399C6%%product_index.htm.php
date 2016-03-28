852547474fb25c0cfc179fb62d728be633b9edf1
tianxiaobao <836369365@qq.com>
2016-03-28 14:05:11 +0800

    update product_index slide

--- templates_c/default/%%2D^2D1^2D1399C6%%product_index.htm.php
+++ templates_c/default/%%2D^2D1^2D1399C6%%product_index.htm.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.20, created on 2016-03-28 13:20:21
+<?php /* Smarty version 2.6.20, created on 2016-03-28 13:54:47
          compiled from product_index.htm */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('insert', 'label', 'product_index.htm', 75, false),)), $this); ?>
@@ -14,7 +14,7 @@ smarty_core_load_plugins(array('plugins' => array(array('insert', 'label', 'prod
 <!--海淘国家 start-->
 <div class="m_country_list">
 <div class="w">
-    <ul class="country_nav newclear" id="country_tab_nav">                                                                                  
+    <ul class="country_nav newclear" id="country_tab_nav">                                                                         
         <li class="item focu"><i class="icon_Japan"></i><span class="mid">日本馆</span></li>
         <li class="item"><i class="icon_Korea_South"></i><span class="mid">韩国馆</span></li>
         <li class="item"><i class="icon_Australia"></i><span class="mid">澳洲馆</span></li>
@@ -103,6 +103,6 @@ var imgdefereds=[];
         imgdefereds.push(dfd);
     });
     $.when.apply(null,imgdefereds).done(function(){
-        $(".slides img").css("margin-left","-276px");
+        $(".slides img").css("margin-left",Math.floor(($(window).width()-1920)/2));
     });
 </script>
