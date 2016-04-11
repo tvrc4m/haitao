88f63a03f2dc866b339b137dac606222055730a5
zhanggaofei <zhgf@mayizaixian.cn>
2016-04-11 17:20:13 +0800

    update

--- module/product/confirm_order.php
+++ module/product/confirm_order.php
@@ -254,9 +254,9 @@ else
 		unset($_SESSION['dist_user_id']);
 		//msg($config['weburl']."/main.php?cg_u_type=1&m=product&s=admin_buyorder");//订单提交成功
 	//	msg($config['pay_url']."/?m=payment&s=pay&tradeNo=".$inorder."&temp=".$config['temp']);//直接跳转到支付页面进行支付选择
+		if($_COOKIE['identity']=='true')
 		msg($config['pay_url']."/?m=payment&s=pay&tradeNo=".$uorder."&temp=".$config['temp']);//直接跳转到支付页面进行支付选择
-		die;
-	}else{
+		else
 		msg($config['pay_url']."/?act=edit&op=name");//实名认证
 		die;
 	}
