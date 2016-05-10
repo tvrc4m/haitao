ccfc5ff3971896faabb57aee40f3bf489116a0c9
huwei <2570328000@qq.com>
2016-05-10 09:07:50 +0800

    adv

--- script/wap_detail.js
+++ script/wap_detail.js
@@ -1,9 +1,10 @@
  var $jq = jQuery.noConflict();
  $jq(function(){
+    var kefu=parseInt($jq(".ub-kefu").css("padding-bottom"));
  	var dScrotop=$jq(".nav-box").offset().top-550;
  	$jq(window).scroll(function(){
         var wScrotop=$jq(document).scrollTop();
-        if(wScrotop>=dScrotop){
+        if(wScrotop>=dScrotop+kefu){
             $jq(".nav-box").addClass("nav-wrapper-test");
         }
         else{
@@ -11,7 +12,7 @@
         }
     })
     $jq(".nav-box ul li").click(function(){
-    	$jq(window).scrollTop(dScrotop);
+    	$jq(window).scrollTop(dScrotop+kefu);
         var liLength=$jq(this).index();
         $jq(this).addClass("active").siblings().removeClass("active");
         $jq(".j_tab").eq(liLength).addClass("pdetail_show").siblings().removeClass("pdetail_show");
