32799e7b2b1c1c6e709a1c1e2b010e3e2598b0fb
zhanggaofei <zhgf@mayizaixian.cn>
2016-05-12 14:32:45 +0800

    up

--- partner.php
+++ partner.php
@@ -2,9 +2,6 @@
 include_once("includes/global.php");
 include_once("includes/smarty_config.php");
 include_once("footer.php");
-$button = isset($_GET['yq'])?1:0;
-$tpl->assign('button',$button);
-$tpl->display('partner.htm');
 if ($config['bw'] == "weixin")
 {
 	include_once("pay/module/payment/lib/WxPayPubHelper/WxPay.pub.config.php");
@@ -38,4 +35,8 @@ if ($config['bw'] == "weixin")
 		$_SESSION['signature'] = sha1($str_tmp);
 	}
 }
+$button = isset($_GET['yq'])?1:0;
+$tpl->assign('button',$button);
+$tpl->display('partner.htm');
+
 ?>
