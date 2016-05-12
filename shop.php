29b7a2be2995b9746bf265b38ffd01f1198c7fa1
zhanggaofei <zhgf@mayizaixian.cn>
2016-05-12 16:01:11 +0800

    up

--- shop.php
+++ shop.php
@@ -37,7 +37,6 @@ if(!$tpl->is_cached("space_temp_inc.htm",$flag))
 			$PluginManager->trigger('analyse', intval($_GET['uid']), 0, $company['company']);
 		}
 	}
-
 	// 如果为3， 则代表分佣及卖家
 	if((1==$company['shop_type'] && $company['shop_statu']==-3) || (2==$company['shop_type'] && $company['shop_statu']==1) || (1==$company['shop_statu'] && 3==$company['shop_type']))
 	{
@@ -166,7 +165,7 @@ if(!$tpl->is_cached("space_temp_inc.htm",$flag))
 			$PluginManager->trigger('dist_product', intval($_GET['uid']));
 
 			//-------------------------------------------
-			if($_GET[fx]='fx')
+			if($_GET[fx]=='fx')
 				$page = "space_index_fx.htm";
 			else
 			$page = "space_index.htm";
