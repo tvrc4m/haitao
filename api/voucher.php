3ce6e9bfc222d4eaecc77a37d8b4b44ce834dc77
zhanggaofei <zhgf@mayizaixian.cn>
2016-05-26 16:16:52 +0800

    up

--- api/voucher.php
+++ api/voucher.php
@@ -8,7 +8,7 @@
 
 class voucher{
 
-    private $_dis = array('100','200','300','500');
+    private $_dis = array('0.5'=>'500','0.3'=>'200','0.2'=>'100');
     /*
      * 获取代金卷列表
      * */
@@ -26,11 +26,30 @@ class voucher{
      * discount  折扣
      * price    面额
      * */
-    public function subDiscount(){ 
+/*    public function subDiscount(){
+    }*/
 
+    /*
+     * 面额生成等比例的代金卷
+     * price    面额
+     * discount     折扣
+     * */
+    public function algorithm($price='',$discount=''){
+
+        if(is_int($price/100)){
+
+            foreach($this->_dis as $k=>$v){
+                $sums[$k] = $price*$k/$this->_dis[$k];
+            }
+            var_dump($sums);
+        }
+    }
+
+    public function price($price,$k){
+        return $price-$price*intval($price*$k/$this->_dis[$k]);
     }
 }
 
 include_once("../includes/global.php");
-
-voucher::showVoucher();
+$obj = new voucher();
+$obj->algorithm('1100');
