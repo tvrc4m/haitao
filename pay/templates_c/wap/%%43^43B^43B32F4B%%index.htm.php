b7d8d0db176e41e182d99431e576f77ba1f88ebd
huwei <2570328000@qq.com>
2016-05-27 17:27:26 +0800

    adv

--- pay/templates_c/wap/%%43^43B^43B32F4B%%index.htm.php
+++ pay/templates_c/wap/%%43^43B^43B32F4B%%index.htm.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.20, created on 2016-05-27 17:12:38
+<?php /* Smarty version 2.6.20, created on 2016-05-27 17:13:25
          compiled from index.htm */ ?>
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => "header.htm", 'smarty_include_vars' => array()));
