450e9b6ad2d1256e43399e90b5f0c7d2986d613b
banweijun <ban@hotmail.com>
2016-06-06 16:12:26 +0800

    update

--- module/sns/includes/plugin_share_class.php
+++ module/sns/includes/plugin_share_class.php
@@ -58,7 +58,7 @@ class share
 		{
 			$sql=" and b.pname like '%$_GET[key]%'";	
 		}
-		$sql="select a.id,a.pid,a.commentcount,b.image,b.pname,b.price,b.collectnum,c.sales,c.price,c.market_price,d.img,d.title from mallbuilder_sns_shareproduct a left join mallbuilder_sns_shareproduct_info b on a.pid = b.pid left join mallbuilder_product c on a.pid=c.id left join mallbuilder_national_pavilions d on c.national = d.id where c.id and a.uid=44 order by a.addtime desc ";
+		$sql="select a.id,a.pid,a.commentcount,b.image,b.pname,b.price,b.collectnum,c.sales,c.price,c.market_price,d.img,d.title from mallbuilder_sns_shareproduct a left join mallbuilder_sns_shareproduct_info b on a.pid = b.pid left join mallbuilder_product c on a.pid=c.id left join mallbuilder_national_pavilions d on c.national = d.id where c.id and a.uid=$buid order by a.addtime desc ";
 		
 		include_once($config['webroot']."/includes/page_utf_class.php");
 		$page = new Page;
