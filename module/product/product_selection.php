015878aea66fa19f8f2e4376b695d66eba11efca
banweijun <ban@hotmail.com>
2016-06-12 13:14:31 +0800

    update

--- module/product/product_selection.php
+++ module/product/product_selection.php
@@ -2,8 +2,16 @@
 global $buid;
 $act = $_REQUEST['act'];
 if(!empty($act) && $act == 'insert'){
-    $id = implode($_POST['id'], ',');
-    $nums = $_POST['nums'];
+    $arr = $_POST['pro'];
+    $id = "";
+    $nums = array();
+    foreach($arr as $key=>$val){
+        if(isset($val['id'])){
+            $id .= $val['id'].',';
+            $nums[] = $val['num'];
+        }
+    }
+    $id = trim($id,',');
     $sql = "select member_id,member_name,catid,type,`name`,subhead,keywords,brand,market_price,price,stock,sales,`code`,pic,pic_more,start_time_type,start_time,valid_time,weight,cubage,province_id,city_id,area_id,street_id,area,freight_id,freight_type,post_price,express_price,ems_price,clicks,rank,uptime,`status`,is_shelves,custom_cat_id,promotion_id,goodbad,shop_rec,is_invoice,is_tg,con,is_virtual,national,is_dist,down_reason,ship_free_id,skuid,trade,pid,ptype from mallbuilder_product where id in($id)";
     $db->query($sql);
     $pro = $db->getRows();
@@ -13,6 +21,8 @@ if(!empty($act) && $act == 'insert'){
         foreach($val as $k => $v){
             if($k=='pid'){
                 $str .= "'".$val['member_id']."',";
+            }elseif($k=='stock'){
+                $str .= $nums[$key].",";
             }elseif($k=='ptype'){
                 $str .= "2,";
             }elseif($k=='member_id'){
@@ -26,17 +36,30 @@ if(!empty($act) && $act == 'insert'){
             }
         }
         $str = trim($str,',');
-        $in_sql = $in_sql.$str.")" ;
+        $in_sql = $in_sql.$str.")";
         $db->query($in_sql);
     }
-    die;
-    var_dump($pro);
+    msg('main.php?m=product&s=product_selection');
 }else {
     //var_dump($_POST);
     //查询店铺所有商品
     $sql = "select id,member_id,member_name,catid,`name`,subhead,brand,market_price,price,stock,pic,national,ptype,pid from mallbuilder_product where member_id = 44 and ptype = 1 ";
+    include_once($config['webroot']."/includes/page_utf_class.php");
+    $page = new Page;
+    $page->url='main.php';
+    $page->listRows = 10;
+    if (!$page->__get('totalRows'))
+    {
+        $db->query($sql);
+        $page->totalRows =$db->num_rows();
+    }
+    $sql .= "  limit ".$page->firstRow.",".$page->listRows;
+    $list['page']=$page->prompt();
+    $list['count']=$page->totalRows;
+    //--------------------------------------------------
     $db->query($sql);
-    $list = $db->getRows();
+    $list['list'] = $db->getRows();
+    //var_dump($list);
     $tpl->assign('products', $list);
     $output = tplfetch("product_selection.htm");
 }
