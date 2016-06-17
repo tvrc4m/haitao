c296b7c8294afdc00c71c9124633642cb040f394
zhanggaofei <zhgf@mayizaixian.cn>
2016-06-17 18:44:41 +0800

    up

--- mem.php
+++ mem.php
@@ -2,7 +2,7 @@
 include_once ("includes/global.php");
 include_once ("includes/uc_server.php");
 $obj = new Uc_server($_SESSION['ucenter_data']);
-var_dump($obj);die;
-$aa = $this->register(array('phone'=>'15011426119','password'=>md5(md5(123456).'abcabc'),'salt'=>'abcabc'));
+
+$aa = $obj->register(array('phone'=>'18717903083','password'=>md5('5d0e2f3ad0c9198b2955740371d26d24abcabc'),'salt'=>'abcabc'));
       var_dump($aa);die;
 ?>
