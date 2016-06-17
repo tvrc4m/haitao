cdb8c1d84653d9d6b5960513d162f2de872ca2f1
zhanggaofei <zhgf@mayizaixian.cn>
2016-06-17 18:15:10 +0800

    up

--- mem.php
+++ mem.php
@@ -1,6 +1,7 @@
 <?php
+include_once ("includes/global.php");
 include_once ("includes/uc_server.php");
-$obj = new Uc_server();
+$obj = new Uc_server($_SESSION['ucenter_data']);
 var_dump($obj);die;
 $aa = $this->register(array('phone'=>'15011426119','password'=>md5(md5(123456).'abcabc'),'salt'=>'abcabc'));
       var_dump($aa);die;
