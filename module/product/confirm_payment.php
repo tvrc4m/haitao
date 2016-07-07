6c9812e5f89e19fb4d021bbe878afb942b275e92
zhanggaofei <zhgf@mayizaixian.cn>
2016-07-07 09:20:12 +0800

    up

--- module/product/confirm_payment.php
+++ module/product/confirm_payment.php
@@ -1,5 +1,35 @@
 <?php
+include_once("module/member/includes/plugin_orderadder_class.php");
+include_once("module/product/includes/plugin_cart_class.php");
+include_once ("$config[webroot]/api/logisticsCost.php");
+$cart = new cart();
 
+if(!empty($_POST['id'])&&!empty($_POST['nums']))
+{
+	if(isset($_POST['is_virtual']) && $_POST['is_virtual'] == 1)
+	{
+		$_SESSION['pid'] = $_POST['id'] * 1;
+		$_SESSION['sid'] = $_POST['sid'] * 1;
+		$_SESSION['is_virtual'] = 1;
+	}
+	else
+	{
+		$flag = $cart->add_cart($_POST['id'],$_POST['nums'],$_POST['sid'], null, $_REQUEST['dist_user_id']);
+		$_SESSION['product_id'] = $flag;
+		$_SESSION['dist_user_id'] = $_REQUEST['dist_user_id'];
+		header("Location: ?m=product&s=confirm_order");
+	}
+}
+
+$orderadder = new orderadder();
+if($config['temp']=='wap'||$config['temp']=='wap_app')
+{
+	$addr = $orderadder -> get_default_orderadder();
+}
+else
+{
+	$addr = $orderadder -> get_orderadderlist();
+}
 include_once("footer.php");
 if($config['temp']=='wap'||$config['temp']=='wap_app')
 {
