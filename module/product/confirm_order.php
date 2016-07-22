91766f45324bbab5b48e7a1b0859df1cbe93ab29
roc <roc@mayizaixian.cn>
2016-07-22 12:16:28 +0800

    change activity logstic

--- module/product/confirm_order.php
+++ module/product/confirm_order.php
@@ -66,7 +66,7 @@ else
 	unset($_POST['product_id']);
 	$_SESSION['product_id'] = $product_id ? $product_id : $_SESSION['product_id'];
 	$_SESSION['dist_user_id'] = $_REQUEST['dist_user_id'] ? $_REQUEST['dist_user_id'] : $_SESSION['dist_user_id'];
-
+	var_dump($_SESSION['product_id']);
 	//修正订单店铺信息
 	$cartlist = $cart -> get_cart_list($on_city,$_SESSION['product_id']);
 	$weig = new logistics($cartlist['weights']);
