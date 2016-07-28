89c322b8637cb54a019fe42b123c783825bb24be
banweijun <ban@hotmail.com>
2016-07-28 15:14:16 +0800

    update

--- module/comactivity/admin/comactivity.php
+++ module/comactivity/admin/comactivity.php
@@ -1,8 +1,8 @@
 <?php
+$option = $_REQUEST;
 
-$option = $_POST['act'];
-
-if($option == 'add'){
+//新增商品活动
+if($option['act'] == 'add'){
 
         $sql = "select id FROM mallbuilder_product WHERE skuid in (".$_POST['sku'].")";
         $db->query($sql);
@@ -23,11 +23,58 @@ if($option == 'add'){
             }
         }
 
-}elseif($option == 'list' || empty($option)){
-    $sql = "SELECT * FROM mallbuilder_commodity_activity ORDER BY begintime";
+//商品信息修改
+}elseif($option['operation'] == 'edit_ads') {
+    if (empty($_POST)) {
+        $sql = "SELECT * FROM mallbuilder_commodity_activity WHERE cid = " . $option['editid'];
+        $db->query($sql);
+        $res = $db->fetchRow();
+        $tpl->assign('de', $res);
+    } else {
+        $sql = "select id FROM mallbuilder_product WHERE skuid in (" . $_POST['sku'] . ")";
+        $db->query($sql);
+        $pid = $db->getRows();
+        if (!empty($pid)) {
+            foreach ($pid as $key => $val) {
+                $newid[] = $val['id'];
+            }
+            $newid = implode(',', $newid);
+            $sql = "UPDATE mallbuilder_commodity_activity SET pid = '{$newid}', sku = '{$_POST[sku]}', begintime = " . strtotime($_POST[begintime]) . ", endtime = " . strtotime($_POST[endtime]) . ", platform = {$_POST[platform]}, position = {$_POST[position]}, `status` = {$_POST[status]} WHERE cid = " . $option['editid'];
+            if ($db->query($sql)) {
+                msg('?m=comactivity&s=comactivity.php');
+            }
+        }
+
+    }
+
+}elseif($option['operation'] == 'del_ads'){
+
+    $cid = is_array($option['chk']) ? implode(',',$option['chk']) : $option['chk'];
+
+    $sql = "DELETE from mallbuilder_commodity_activity WHERE cid in ($cid)";
+    if($db->query($sql)){
+        msg('?m=comactivity&s=comactivity.php');
+    }
+//活动商品展示
+}elseif($option['act'] == 'list' || empty($option['act'])){
+
+    $where = !empty($option['name']) ? "WHERE LOCATE($option[name],sku)" : null ;
+    include_once("$config[webroot]/includes/page_utf_class.php");
+    $sql = "SELECT * FROM mallbuilder_commodity_activity $where ORDER BY begintime DESC ";
+
+    $page = new Page;
+    $page->listRows=20;
+    //分页
+    if (!$page->__get('totalRows'))
+    {
+        $db->query($sql);
+        $page->totalRows = $db->num_rows();
+    }
+    $sql .= "  limit ".$page->firstRow.",".$page->listRows;
+
     $db->query($sql);
     $res = $db->getRows();
-
+    $tpl->assign('page',$page->prompt());
     $tpl->assign('de',$res);
     /*foreach($res as $key => $val){
         $sql = "select name,price,pic FROM mallbuilder_product WHERE id in(".$val['pid'].")";
@@ -35,6 +82,8 @@ if($option == 'add'){
         $res[$key]['product'] = $db->getRows();
 
    }*/
-
 }
+//修改活动商品
+
+
 $tpl->display("comactivity.htm");
