fa3062d6aa9ba3fe76a46c70700ad28347e05d40
roc <roc@mayizaixian.cn>
2016-07-29 14:59:50 +0800

    change wechat auth

--- index.php
+++ index.php
@@ -3,66 +3,68 @@ include_once("includes/global.php");
 include_once("includes/smarty_config.php");
 // ========= 微信支付第一步骤 =========
 
+/*
 if($config['bw'] == "weixin" && (!isset($_SESSION['openid_f']) || $_SESSION['openid_f']==""))
 {
 	/**
 	 * 成功调起支付第一步骤：
 	 * 步骤1：网页授权获取用户openid
 	*/
-	include_once("./pay/module/payment/lib/WxPayPubHelper/WxPayPubHelper.php");
-	//使用jsapi接口
-	$jsApi = new JsApi_pub();
+// 	include_once("./pay/module/payment/lib/WxPayPubHelper/WxPayPubHelper.php");
+// 	//使用jsapi接口
+// 	$jsApi = new JsApi_pub();
 
-	//通过code获得openid
-	if (!isset($_GET['code']) && (!isset($_SESSION['openid_f']) || $_SESSION['openid_f']=="")) // && $_GET['m']!="product"
-	{
-		//$url_temp = WxPayConf_pub::JS_API_CALL_URL;
+// 	//通过code获得openid
+// 	if (!isset($_GET['code']) && (!isset($_SESSION['openid_f']) || $_SESSION['openid_f']=="")) // && $_GET['m']!="product"
+// 	{
+// 		//$url_temp = WxPayConf_pub::JS_API_CALL_URL;
 
-		/**
-		* roc 2016.07.27 start---
-		$url_temp = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		*/
-		$url_temp = $_SERVER['HTTP_REFERER'];
-        if(empty($url_temp))
-        {
-               $url_temp= $config['weburl'].'/main.php?cg_u_type=1';
-        }
-        else
-        {
-        	if(!preg_match("/^".str_replace("/", "\/", $config['weburl'])."*/",$url_temp))
-        	{
-        		$url_temp = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        	}
+// 		/**
+// 		* roc 2016.07.27 start---
+// 		$url_temp = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+// 		*/
+// 		$url_temp = $_SERVER['HTTP_REFERER'];
+//         if(empty($url_temp))
+//         {
+//                $url_temp= $config['weburl'].'/main.php?cg_u_type=1';
+//         }
+//         else
+//         {
+//         	if(!preg_match("/^".str_replace("/", "\/", $config['weburl'])."*/",$url_temp))
+//         	{
+//         		$url_temp = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//         	}
 
-        }
-		/**
-		* roc 2016.07.27 end---
-		$url_temp = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		*/
-		$url_temp = urlencode($url_temp);
+//         }
+// 		/**
+// 		* roc 2016.07.27 end---
+// 		$url_temp = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+// 		*/
+// 		$url_temp = urlencode($url_temp);
 
-		//触发微信返回code码
-		$url = $jsApi->createOauthUrlForCode($url_temp);
+// 		//触发微信返回code码
+// 		$url = $jsApi->createOauthUrlForCode($url_temp);
 		
-		header("Location: $url");
-	}
-	else if(isset($_GET['code']))
-	{
-		//获取code码，以获取openid
-	    $code = $_GET['code'];
-		$jsApi->setCode($code);
-		$openid = $jsApi->getOpenId();
-		$_SESSION['openid_f'] = $openid;
-		//自动根据openid登录操作
-	}
+// 		header("Location: $url");
+// 	}
+// 	else if(isset($_GET['code']))
+// 	{
+// 		//获取code码，以获取openid
+// 	    $code = $_GET['code'];
+// 		$jsApi->setCode($code);
+// 		$openid = $jsApi->getOpenId();
+// 		$_SESSION['openid_f'] = $openid;
+// 		//自动根据openid登录操作
+// 	}
 
-}
+// }
+
 //==========================================
 $dre=explode(".",$_SERVER['HTTP_HOST']);
 $dir=trim(dirname($_SERVER['SCRIPT_NAME']), '\,/');//view: abc.**.com/a.php
 
 $true_prefix=str_replace($config['baseurl'],'',$_SERVER['HTTP_HOST']);
-$original_prefix=str_replace('http://','',str_replace($config['baseurl'],'',$config['weburl']));
+$original_prefix=str_replace($config['http_type'],'',str_replace($config['baseurl'],'',$config['weburl']));
 
 //view:abc.abc.com, baserurl:abc.abc.com, weburl:abc.abc.com ----- true_prefix='';original_prefix='';
 //view:abc.ocm view, baseurl:abc.com, weburl:www.abc.com true_prefix='',original_prefix='www'
@@ -78,7 +80,7 @@ if($true_prefix!=$original_prefix&&empty($dpid)&&empty($dcid)&&empty($dir)&&!emp
 	if($config['opensuburl'])
 	{
 		//绑定顶级域名
-		$sql="select userid from ".SHOP." where domin='"."http://".$_SERVER['HTTP_HOST']."'";
+		$sql="select userid from ".SHOP." where domin='".$config['http_type'].$_SERVER['HTTP_HOST']."'";
 		$db->query($sql);
 		$re=$db->fetchRow();
 		if($re['userid'])
