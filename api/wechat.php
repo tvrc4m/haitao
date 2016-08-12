838ae7a007ea54f20203620c1537901d87b2d726
roc <roc@mayizaixian.cn>
2016-08-12 16:21:52 +0800

    add change wechat suscribe reply

--- api/wechat.php
+++ api/wechat.php
@@ -44,57 +44,51 @@ class wechatCallbackapiTest
 				$str="";
 
 				Yf_Log::log('$postObj : ' . json_encode($postObj), Yf_Log::INFO, 'wechat');
-				if($RX_TYPE=='text')
-				{
-					if(!empty($keyword))
-					{
-						$uid = $_GET['uid'];
-						if($uid)
+				switch ($RX_TYPE) {
+					case 'text':
+						# code...
+						if(!empty($keyword))
 						{
-							$str = " and member_id ='$uid' ";	
-						}								
-						$sql="select name as pname,id,pic from ".PRODUCT." where name like '%$keyword%' $str order by id desc limit 0,4";
-						$db->query($sql);
-						$re=$db->getRows();
-						foreach($re as $val)
-						{
-							$str.="<item>
-								 <Title><![CDATA[".$val['pname']."]]></Title> 
-								 <Description><![CDATA[]]></Description>
-								 <PicUrl><![CDATA[".$val['pic']."]]></PicUrl>
-								 <Url><![CDATA[".$config['weburl']."?m=product&s=detail&id=".$val['id']."]]></Url>
-							</item>";
-							$num++;
-						}
-						if($num>0)
-						{
-							$textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[news]]></MsgType>
-							<ArticleCount>".$num."</ArticleCount>
-							<Articles>".$str."</Articles>
-							<FuncFlag>1</FuncFlag>
-							</xml>"; 
-							$msgType = "text";
-							$contentStr = "";
-							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-							echo $resultStr;
-						}
-					}
-					else
-						echo "Input something...";
-				}
-				elseif ($RX_TYPE == 'event')
-				{
-					echo "Input something...";
-				}
-				else
-				{
+							$uid = $_GET['uid'];
+							if($uid)
+							{
+								$str = " and member_id ='$uid' ";	
+							}								
+							$sql="select name as pname,id,pic from ".PRODUCT." where name like '%$keyword%' $str order by id desc limit 0,4";
+							$db->query($sql);
+							$re=$db->getRows();
+							$msg_arr = array();
+							foreach($re as $val)
+							{
+								$msg_arr[] = array("title"=>$val['pname'],"description"=>"","picurl"=>$val['pic'],"url"=>$config['weburl']."?m=product&s=detail&id=".$val['id']);
+							}
 
-                	echo "Input something...";
-                }
+							echo $this->sendTextImage($postObj,$msg_arr);
+						}
+						else
+							echo "Input something...";
+						break;
+					case 'event':
+						$Event = $postObj->Event;
+						switch ($Event) {
+							case 'subscribe':
+								echo $this->sendText($postObj,$msg_arr);
+								break;
+							default:
+								# code...
+								break;
+						}
+						break;
+					case 'variable':
+						# code...
+						break;
+					
+					default:
+						# code...
+						break;
+
+				}
+			
         }else{
         	echo "";
         	exit;
@@ -118,5 +112,47 @@ class wechatCallbackapiTest
 			return false;
 		}
 	}
+
+	private function sendText($postObj, $msg)
+    {
+
+        $returnStr = '<xml>
+                            <ToUserName><![CDATA[' . $postObj->FromUserName . ']]></ToUserName>
+                            <FromUserName><![CDATA[' . $postObj->ToUserName . ']]></FromUserName>
+                            <CreateTime>' . time() . '</CreateTime>
+                            <MsgType><![CDATA[text]]></MsgType>
+                            <Content><![CDATA[' . $msg['description'] . ']]></Content>
+                            </xml>';
+        return $returnStr ? $returnStr : '';
+    }
+    private function sendTextImage($postObj, $msg)
+    {
+
+        $newsTplHead = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[news]]></MsgType>
+                        <ArticleCount>%s</ArticleCount>
+                        <Articles>";
+        $newsTplBody = "<item>
+                        <Title><![CDATA[%s]]></Title> 
+                        <Description><![CDATA[%s]]></Description>
+                        <PicUrl><![CDATA[%s]]></PicUrl>
+                        <Url><![CDATA[%s]]></Url>
+                        </item>";
+        $newsTplFoot = "</Articles></xml>";
+        $bodyCount = count($msg);
+        $bodyCount = $bodyCount < 10 ? $bodyCount : 10;
+        $header = sprintf($newsTplHead, $postObj->FromUserName, $postObj->ToUserName, time(), $bodyCount);
+        foreach ($msg as $key => $value) {
+            $body .= sprintf($newsTplBody, $value['title'], $value['description'], $value['picurl'], $value['url']);
+        }
+
+        $FuncFlag = 0;
+        $footer = sprintf($newsTplFoot, $FuncFlag);
+        return $header . $body . $footer;
+
+    }
 }
 ?>
