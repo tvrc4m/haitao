1085e50127e1af3e5beab0e0e3be76f723139d40
roc <roc@mayizaixian.cn>
2016-08-12 16:48:33 +0800

    add change wechat suscribe reply

--- api/wechat.php
+++ api/wechat.php
@@ -19,6 +19,7 @@ else
 
 class wechatCallbackapiTest
 {
+	public $postObj;
 	public function valid()
     {
         $echoStr = $_GET["echostr"];
@@ -30,104 +31,123 @@ class wechatCallbackapiTest
     }
     public function responseMsg()
     {
+
+
+    	$postStr = file_get_contents("php://input");
+        if (!empty($postStr)) {
+            // 解析微信传过来的 XML 内容
+            $this->postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            //      file_put_contents("./1.txt",var_export($this -> postObj,true));
+            $fromUser = $this->postObj->FromUserName; //发送方帐号（一个OpenID）
+            // $toUser = $this->postObj->ToUserName;//开发者微信号
+            $msgType = $this->postObj->MsgType; //消息类型
+            switch ($msgType) {
+                case 'text':
+
+                    $keyword = trim($this->postObj->Content); //用户发送的消息
+                    $this->textMsg($keyword, $fromUser);
+                    break;
+                case 'image':
+
+                    $this->imageMsg();
+                    break;
+                case 'voice':
+                    $this->voiceMsg();
+                    break;
+                case 'video':
+                    $this->videoMsg();
+                    break;
+                case 'location':
+                    $this->locationMsg();
+                    break;
+                case 'link':
+                    $this->linkMsg();
+                    break;
+                case 'event':
+
+                    $this->eventMsg();
+                    break;
+                default:
+
+                    echo 'Error!';
+                    exit;
+                    break;
+            }
+            // $keyword = trim($postObj->Content); // $keyword 就是用户输入的内容
+        } else {
+            echo 'Error!';
+            exit;
+        }
+    }
+	private function textMsg($keyword, $fromUser)
+	{
+
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 		Yf_Log::log('$postStr : ' . $postStr, Yf_Log::INFO, 'wechat');
 		global $db,$config;
-		if (!empty($postStr)){
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-				$RX_TYPE = trim($postObj->MsgType);
-				$num=0;
-				$str="";
 
-				Yf_Log::log('$postObj : ' . json_encode($postObj), Yf_Log::INFO, 'wechat');
-				switch ($RX_TYPE) {
-					case 'text':
-						# code...
-						if(!empty($keyword))
-						{
-							$uid = $_GET['uid'];
-							if($uid)
-							{
-								$str = " and member_id ='$uid' ";	
-							}								
-							$sql="select name as pname,id,pic from ".PRODUCT." where name like '%$keyword%' $str order by id desc limit 0,4";
-							$db->query($sql);
-							$re=$db->getRows();
-							$msg_arr = array();
-							foreach($re as $val)
-							{
-								$msg_arr[] = array("title"=>$val['pname'],"description"=>"","picurl"=>$val['pic'],"url"=>$config['weburl']."?m=product&s=detail&id=".$val['id']);
-							}
-
-							echo $this->sendTextImage($postObj,$msg_arr);
-						}
-						else
-							echo "Input something...";
-						break;
-					case 'event':
-						$Event = $postObj->Event;
-						switch ($Event) {
-							case 'subscribe':
-								echo $this->sendTextImage($postObj,
-									array(array('title' =>"欢迎关注蚂蚁海淘" , "description"=>"很不错的平台","picurl"=>"https://www.mayihaitao.com/uploadfile/adv/2016/04/28/1461831474.jpg","url"=>$config['weburl']."?m=product&s=detail&id=1"))
-									);
-								break;
-							default:
-								# code...
-								break;
-						}
-						break;
-					case 'variable':
-						# code...
-						break;
-					
-					default:
-						# code...
-						break;
-
-				}
-			
-        }else{
-        	echo "";
-        	exit;
-        }
-    }
-	private function checkSignature()
-	{
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];	
-        		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
-		sort($tmpArr);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
+		if(!empty($keyword))
+		{
+			$uid = $_GET['uid'];
+			if($uid)
+			{
+				$str = " and member_id ='$uid' ";	
+			}								
+			$sql="select name as pname,id,pic from ".PRODUCT." where name like '%$keyword%' $str order by id desc limit 0,4";
+			$db->query($sql);
+			$re=$db->getRows();
+			$msg_arr = array();
+			foreach($re as $val)
+			{
+				$msg_arr[] = array("title"=>$val['pname'],"description"=>"","picurl"=>$val['pic'],"url"=>$config['weburl']."?m=product&s=detail&id=".$val['id']);
+			}
+			echo $this->sendTextImage($msg_arr);
 		}
 	}
+	private function imageMsg(){}
+	private function voiceMsg(){}
+	private function videoMsg(){}
+	private function locationMsg(){}
+	private function linkMsg(){}
+	private function eventMsg(){
+		$Event = $this->postObj->Event;
+        $fromUser = trim($this->postObj->FromUserName); //发送方帐号（一个OpenID）
+        switch ($Event) {
+            case 'CLICK':// 用户点击菜单
+                $keyword = trim($this->postObj->EventKey);
+                $this->textMsg($keyword, $fromUser);
+                break;
+            case 'LOCATION':// 上报地理位置
+                $this->locationMsg();
+                break;
+            case 'subscribe':// 关注后消息
+				echo $this->sendTextImage(array(array('title' =>"欢迎关注蚂蚁海淘" , "description"=>"很不错的平台","picurl"=>"https://www.mayihaitao.com/uploadfile/adv/2016/04/28/1461831474.jpg","url"=>$config['weburl']."?m=product&s=detail&id=1"))
+					);
+                break;
+            case 'unsubscribe':
 
-	private function sendText($postObj, $msg)
+                break;
+            case 'SCAN':// 用户本来已经关注过，扫描其他二维码进入时，回复的消息
+                break;
+            default:
+                break;
+        }
+
+
+	}
+	private function sendText($msg)
     {
 
         $returnStr = '<xml>
-                            <ToUserName><![CDATA[' . $postObj->FromUserName . ']]></ToUserName>
-                            <FromUserName><![CDATA[' . $postObj->ToUserName . ']]></FromUserName>
+                            <ToUserName><![CDATA[' . $this->postObj->FromUserName . ']]></ToUserName>
+                            <FromUserName><![CDATA[' . $this->postObj->ToUserName . ']]></FromUserName>
                             <CreateTime>' . time() . '</CreateTime>
                             <MsgType><![CDATA[text]]></MsgType>
                             <Content><![CDATA[' . $msg . ']]></Content>
                             </xml>';
         return $returnStr ? $returnStr : '';
     }
-    private function sendTextImage($postObj, $msg)
+    private function sendTextImage($msg)
     {
 
         $newsTplHead = "<xml>
@@ -146,7 +166,7 @@ class wechatCallbackapiTest
         $newsTplFoot = "</Articles></xml>";
         $bodyCount = count($msg);
         $bodyCount = $bodyCount < 10 ? $bodyCount : 10;
-        $header = sprintf($newsTplHead, $postObj->FromUserName, $postObj->ToUserName, time(), $bodyCount);
+        $header = sprintf($newsTplHead, $this->postObj->FromUserName, $this->postObj->ToUserName, time(), $bodyCount);
         foreach ($msg as $key => $value) {
             $body .= sprintf($newsTplBody, $value['title'], $value['description'], $value['picurl'], $value['url']);
         }
@@ -156,5 +176,25 @@ class wechatCallbackapiTest
         return $header . $body . $footer;
 
     }
+
+
+    private function checkSignature()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
 ?>
