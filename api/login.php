b3f5edeaac2bbbbcc249c21dc982e0dc81888045
zhanggaofei <zhgf@mayizaixian.cn>
2016-08-16 15:05:53 +0800

    up

--- api/login.php
+++ api/login.php
@@ -20,40 +20,47 @@ class login
 	private $_userinfo = '';//存储用户信息
 	private $_action = '';//请求方法名
 	private $_response_code = '';//回调状态
-	private $_response_data = '';//回调数据
+	private $_response_data = null;//回调数据
 	private $_error = array(
         '00000'=>'登录成功！',
         '10001'=>'亲！玩我呢！',
         '10002'=>'请求的方法不存在！',
         '10003'=>'请输入帐号和密码',
         '10004'=>'请输入帐号',
-        '10005'=>'帐号与密码不匹配'
+        '10005'=>'请输入密码',
+        '10006'=>'帐号与密码不匹配',
+        '10007'=>'帐号已锁！'
     );
 
 	public function __construct(){
         global $config,$db;
-        
-		$post = !empty($_POST)?$_POST:$this->_response_code='10001';
+
+		$post = !empty($_REQUEST)?$_REQUEST:$this->_response_code='10001';
+		$this->_action = $post['action'];
+		if($this->_action == 'login'){
 		if (empty($post['username'])) $this->_response_code='10004'; else $this->_account = $post['username'];
-		if (empty($post['password'])) $this->_response_code='10005'; else $this->_password = $post['password'];
+		if (empty($post['password'])) $this->_response_code='10005'; else $this->_password = md5($post['password']);
 		
+		}
 		if(!empty($this->_account) && !empty($this->_password)){
 			
 			$this->_config = $config;
-       		$this->db = $db;
+       		$this->_db = $db;
+       		
 			if (method_exists($this,$this->_action)) {
                 call_user_func(array($this,$this->_action));
             }else{
                 // 请求的方法不存在
-                $this->_response_code='10001';
+                $this->_response_code='10002';
             }
 		}
-		
+
         $this->response();
 	}
 
 	private function response(){
-		echo json_decode(array('status'=>$this->_response_code,'errmsg'=>$this->_error[$this->_response_code],'data'=>$this->_response_data));die;
+		var_dump(array('status'=>$this->_response_code,'errmsg'=>$this->_error[$this->_response_code],'data'=>$this->_response_data));die;
+		//echo json_decode();die;
 
 	}
 	/*
@@ -62,15 +69,29 @@ class login
 	 *
 	 *return 状态 
 	 **/
-	private function login($users=''){
-		
+	private function login(){
 
+		global $post,$config;
+
+	    $sql="select userid,user,statu,pid,password from ".MEMBER." where mobile = {$this->_account}";
+	    $this->_db->query($sql);
+	    $re=$this->_db->fetchRow();
+	    if(substr($re['password'],0,4)=='lock')$this->_response_code = '10007';
+	    if ($this->_password==$re['password']) {
+	    	bsetcookie("USERID",$re['userid']."\t".$re['user']."\t".$re['pid'],NULL,"/",$this->_config['baseurl']);
+
+			$sql="update ".MEMBER." set lastLoginTime='".time()."' WHERE userid='{$re['userid']}'";
+			$this->_db->query($sql);
+	    	$this->_response_code = '00000';
+	    }else{
+			$this->_response_code = '10006';
+	    }
+	    
 	}
-
-	/*
-	*注册
-	* 
-	*/
+	/**
+	 * [register description]
+	 * @return [type] [description]
+	 */
 	private function register(){
 
 	}
