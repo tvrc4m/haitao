90602f9cf6ea8415843be4c92b248aa6c4736a40
tianxiaobao <836369365@qq.com>
2016-08-18 17:37:24 +0800

    up

--- script/pc/login.js
+++ script/pc/login.js
@@ -38,7 +38,19 @@ define(function (require,exports,module) {
 	Check.prototype.regist = function() {
 		var _self = this;
 		$(".button").click(function(){
-			$.post("register.php",{mobile:$("#mobile").val(),smsvode:$("#smsvode").val(),password:$("#password").val()},function(msg){
+			$.post("/api/login.php",{mobile:$("#mobile").val(),smsvode:$("#smsvode").val(),password:$("#password").val()},function(msg){
+				var data = JSON.parse(msg);
+				_self.setTip(data.errmsg);
+			});
+		});
+	};
+	/*
+	*找回密码
+	*/
+	Check.prototype.lostpass = function() {
+		var _self = this;
+		$(".button").click(function(){
+			$.post("/api/login.php",{mobile:$("#mobile").val(),smsvode:$("#smsvode").val(),password:$("#password").val()},function(msg){
 				var data = JSON.parse(msg);
 				_self.setTip(data.errmsg);
 			});
@@ -47,11 +59,12 @@ define(function (require,exports,module) {
 	/*
 	*获取验证码
 	*/
-	Check.prototype.idCode = function(){
+	Check.prototype.idCode = function(from){
 		var _self = this;
 		var num = 60;
+		var type = from ? from : "lostpass"
 	    $(".idcode .btn").click(function(){
-	    	$.post("/api/login.php",{username:$("#mobile").val(),action:"yzCode"},function(msg){
+	    	$.post("/api/login.php",{username:$("#mobile").val(),action:"yzCode",type:type},function(msg){
 	    		var data = JSON.parse(msg);
 				_self.setTip(data.errmsg);
 				if(data.status == '10017'){
@@ -71,6 +84,9 @@ define(function (require,exports,module) {
 			});
 	    });
 	};
+	/*
+	*错误提示
+	*/
 	Check.prototype.setTip = function(s){
 		$(".tips").text(s).css("display","block");
 	}
