75b879ba3a10b1f87e16bee0efacd0357cde23b3
zhanggaofei <zhgf@mayizaixian.cn>
2016-08-18 15:28:32 +0800

    up

--- api/login.php
+++ api/login.php
@@ -12,18 +12,19 @@ include("./verification.php");
 class login extends verification
 {
 
-	public $_account = '';//用户
+	protected $_account = '';//用户
 	private $_password = '';//密码
 	private $_password_old = '';//旧密码
 	private $_yzm = '';
+	protected $_yzm_mobile = '';
 	private $_users = '';
-	private $_config = '';
+	protected $_config = '';
 	private $_db = '';
 	private $_userinfo = '';//存储用户信息
-	private $_action = '';//请求方法名
-	private $_response_code = '';//回调状态
-	private $_response_data = null;//回调数据
-	private $_error = array(
+	protected $_action = '';//请求方法名
+	protected $_response_code = '';//回调状态
+	protected $_response_data = null;//回调数据
+	protected $_error = array(
         '00000'=>'登录成功！',
         '10001'=>'亲！玩我呢！',
         '10002'=>'请求的方法不存在！',
@@ -41,43 +42,43 @@ class login extends verification
         '10014'=>'密码修改成功！',
         '10015'=>'密码修改失败！',
         '10016'=>'密码不对！',
-        '10017'=>'短信发送成功！'
+        '10017'=>'短信发送成功，请注意查收!',
+        '10018'=>'操作过于频繁!',
+        '10019'=>'请再次申请短信验证码',
+        '10020'=>'请填写正确的验证码!',
+        '10021'=>'验证码已失效!'
     );
 
 	public function __construct(){
-		parent::__construct();
+		
         global $config,$db;
 
 		$post = !empty($_REQUEST)?$_REQUEST:$this->_response_code='10001';
 		$this->_action = $post['action'];
+
 		if (empty($post['username'])) $this->_response_code='10004'; else $this->_account = $post['username'];
 		if (empty($post['password'])) $this->_response_code='10005'; else $this->_password = md5(addslashes($post['password']));
 		if (!empty($post['password_old'])&&$post['action']=='updatepass')$this->_password_old = md5(addslashes($post['password_old']));
+		if(empty($post['smsvode']))$this->_response_code = '10020'; else $this->_yzm = $post['smsvode'];
 
-		$this->aa->yzm();die;
+		$this->_yzm_mobile = 'mon_yzm_'.$this->_account;
+		
 		$this->_config = $config;
    		$this->_db = $db;
    		$status = false;
-   		if($this->checkData($this->_account,'mobile')){
-   			if(empty($post['smsvode']))
-   				$status = $this->users(); 
-			else{
-				parent::yzm();
-				$this->_response_code = '10017';
-			}
 
-   		}else $this->_response_code = '10010'; 
+   		if(parent::checkData($this->_account,'mobile')){
 
-		if($status){
-			if (method_exists($this,$this->_action)) {
-                call_user_func(array($this,$this->_action));
-            }else{
-                // 请求的方法不存在
-                $this->_response_code='10002';
-            }
-		}else
-		$this->_response_code = '10013';
-			
+   			$this->users();
+			if(!empty($this->_account)){
+				if (method_exists($this,$this->_action)) {
+	                call_user_func(array($this,$this->_action));
+	            }else{
+	                // 请求的方法不存在
+	                $this->_response_code='10002';
+	            }
+			} 
+		}else $this->_response_code = '10010';
 
         $this->response();
 	}
@@ -114,11 +115,23 @@ class login extends verification
 	 */
 	private function register(){
 		if(!empty($this->_users['mobile'])){$this->_response_code = '10009';return false;}
-		$type = $this->doreg();
-		if($type)
-			$this->_response_code = '10011';
-		else
-			$this->_response_code = '10012';
+
+        if($this->_yzm==$_SESSION[$this->_yzm_mobile]['yzm']){
+			if($_SESSION[$this->_yzm_mobile]['ytime']<time()){
+				$this->_response_code = '10021';
+			}else{
+				$type = $this->doreg();
+				if($type){
+					$this->_response_code = '10011';
+					session_unset($_SESSION[$this->_yzm_mobile]);
+				}else
+					$this->_response_code = '10012';
+				
+	        }
+		}else{
+			$this->_response_code = '10020';
+		}
+		return false;
 	}
 
 	/*
@@ -127,11 +140,23 @@ class login extends verification
 	*/
 	private function lostpass(){
 		if(empty($this->_users['mobile'])){$this->_response_code = '10013';return false;}
-		$status = $this->update_pwd();
-		if ($status)
-			$this->_response_code = '10014';
-		else
-			$this->_response_code = '10015';
+
+        if($this->_yzm==$_SESSION[$this->_yzm_mobile]['yzm']){
+			if($_SESSION[$this->_yzm_mobile]['ytime']<time()){
+				$this->_response_code = '10021';
+			}else{
+				$status = $this->update_pwd();
+				if ($status){
+					$this->_response_code = '10014';
+					session_unset($_SESSION[$this->_yzm_mobile]);
+				}else
+					$this->_response_code = '10015';
+	        }
+		}else{
+			$this->_response_code = '10020';
+		}
+
+		return false;
 	}
 
 	/*
@@ -149,52 +174,20 @@ class login extends verification
 			else
 				$this->_response_code = '10015';
 		}
+
+		return false;
 	}
 
 	/**
-	 * 数据格式验证
+	 * 验证码
 	 */
-	private function checkData($data = null, $keyval = null){
-	    $res = null;
-	    switch($keyval){
-	        case 'user' : $res = preg_match('/^[A-Za-z0-9\x{4e00}-\x{9fa5}]{4,16}$/u', $data);  break;
-	        case 'mobile' : $res = preg_match('/^1(3[0-9]|4[57]|5[0-35-9]|8[0-9]|7[07])\d{8}$/', $data);  break;
-	        case 'smsvode' : $res = preg_match('/^[0-9]{6}$/', $data);  break;
-	        case 'password' : $res = preg_match('/^[\s\S]{6,16}$/', $data);  break;
-	    }
-	    return $res;
+	private function yzCode(){
+
+		parent::yzm();
+
+		return false;
 	}
-
-	/**
-	 * [rand_pwd description]
-	 * @return [type] [description]
-	 */
-	public function rand_pwd(){
-		$str = null;
-		$strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-		$max = strlen($strPol)-1;
-
-		for($i=0;$i<6;$i++){
-			$str.=$strPol[rand(0,$max)];
-		}
-
-		return $str;
-	}
-
-	//短信发送
-	public function Send_msg($mob = null, $con = null)
-	{
-		global $config;
-		include_once("{$this->_config['webroot']}/module/sms/includes/plugin_sms_class.php");
-		$sms = new sms();
-		$str = $sms->send($mob, $con);
-		$res = json_decode(iconv("gb2312", "utf-8//IGNORE", urldecode($str)),true);
-		if($res['error']==0&&$res['msg']=='ok'){
-			return 1;
-		}else{
-			return 2;
-		}
-	}
+	
 
 	/**
 	 * [users description]
@@ -227,6 +220,8 @@ class login extends verification
 
 		$sql="update ".MEMBER." set lastLoginTime='".time()."' WHERE userid='{$this->_users['userid']}'";
 		$this->_db->query($sql);
+
+		return false;
 	}
 	/**
 	 * [doreg description]
--- api/verification.php
+++ api/verification.php
@@ -2,45 +2,54 @@
 /**
  * User: wonder
  * describe:
- * 		功能：发送验证码
+ * 		功能：发送验证码、验证
  */
 class verification
 {
-	private $_response_code = '';//回调状态
-	private $_response_data = null;//回调数据
-	private $_error = array(
-        '00000'=>'登录成功！',
-        '10001'=>'亲！玩我呢！',
-        '10002'=>'请求的方法不存在！',
-        '10003'=>'请输入帐号和密码',
-        '10004'=>'请输入帐号',
-        '10005'=>'请输入密码',
-        '10006'=>'帐号与密码不匹配',
-        '10007'=>'帐号已锁！',
-        '10008'=>'短信发送成功！',
-        '10009'=>'该手机号已存在！',
-        '10010'=>'请填正确的手机号！',
-        '10011'=>'注册成功！',
-        '10012'=>'注册失败！',
-        '10013'=>'用户不存在！',
-        '10014'=>'密码修改成功！',
-        '10015'=>'密码修改失败！',
-        '10016'=>'密码不对！',
-        '10017'=>'短信发送成功！'
-    );
 
-	public function __construct(){
-		
-			
-
-        $this->response();
-
+	/**
+	 * 数据格式验证
+	 */
+	public function checkData($data = null, $keyval = null){
+	    $res = null;
+	    switch($keyval){
+	        case 'user' : $res = preg_match('/^[A-Za-z0-9\x{4e00}-\x{9fa5}]{4,16}$/u', $data);  break;
+	        case 'mobile' : $res = preg_match('/^1(3[0-9]|4[57]|5[0-35-9]|8[0-9]|7[07])\d{8}$/', $data);  break;
+	        case 'smsvode' : $res = preg_match('/^[0-9]{6}$/', $data);  break;
+	        case 'password' : $res = preg_match('/^[\s\S]{6,16}$/', $data);  break;
+	    }
+	    return $res;
 	}
 
-	private function response(){
-		var_dump(array('status'=>$this->_response_code,'errmsg'=>$this->_error[$this->_response_code],'data'=>$this->_response_data));die;
-		//echo json_decode();die;
+	/**
+	 * [rand_pwd description]
+	 * @return [type] [description]
+	 */
+	public function rand_pwd(){
+		$str = null;
+		$strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+		$max = strlen($strPol)-1;
 
+		for($i=0;$i<6;$i++){
+			$str.=$strPol[rand(0,$max)];
+		}
+
+		return $str;
+	}
+
+	//短信发送
+	public function Send_msg($mob = null, $con = null)
+	{
+		global $config;
+		include_once("{$this->_config['webroot']}/module/sms/includes/plugin_sms_class.php");
+		$sms = new sms();
+		$str = $sms->send($mob, $con);
+		$res = json_decode(iconv("gb2312", "utf-8//IGNORE", urldecode($str)),true);
+		if($res['error']==0&&$res['msg']=='ok'){
+			return 1;
+		}else{
+			return 2;
+		}
 	}
 
 	/**
@@ -48,34 +57,35 @@ class verification
 	 * @return [type] [description]
 	 */
 	public function yzm(){
-		 $this->_response_code='10002';
-		return '111';
-		 if(checkData($_POST['mobile'], 'mobile')){
-         if(empty($_SESSION['mon_yzm'])||$_SESSION['mon_yzm']['ltime']<time()) {
-            if($_SESSION['mon_yzm']['lasttime']<=time()){
-                $_SESSION['mon_yzm']['lnum'] = 1;
-            }
-            if(empty($_SESSION['mon_yzm']['lnum'])||$_SESSION['mon_yzm']['lnum']<=3) {
-                $number = rand(100000,999999);
-                if (Send_msg($_POST['mobile'], sprintf('您本次注册蚂蚁海淘的验证码是%s有效期为%s分钟', $number, 10)) == 1) {
+	
 
-                    if(date('i',time()-$_SESSION['mon_yzm']['ltime'])<5){
-                        $vser['lnum'] =  $_SESSION['mon_yzm']['lnum']+1;
-                    }
+		if(empty($_SESSION[$this->_yzm_mobile])||$_SESSION[$this->_yzm_mobile]['ltime']<time()) {
+            if($_SESSION[$this->_yzm_mobile]['lasttime']<=time()){
+                $_SESSION[$this->_yzm_mobile]['lnum'] = 1;
+            }
+            if(empty($_SESSION[$this->_yzm_mobile]['lnum'])||$_SESSION[$this->_yzm_mobile]['lnum']<=3){
+                $number = rand(100000,999999);
+                if ($this->Send_msg($this->_account, sprintf('您本次注册蚂蚁海淘的验证码是%s有效期为%s分钟', $number, 10)) == 1) {
+
+                    $vser['lnum'] =  $_SESSION[$this->_yzm_mobile]['lnum']+1;
                     $vser['yzm'] = $number;
                     $vser['ytime'] = time() + 60 * 10;
                     $vser['ltime'] = time() + 60;
                     $vser['lasttime'] = time() + 60 * 60;
-                    $_SESSION['mon_yzm'] = $vser;
-                    die(Return_data(array('status_code' => '200', 'message' => '短信发送成功，请注意查收', 'data' => null )));
+                    $_SESSION[$this->_yzm_mobile] = $vser;
+                    $this->_response_code = '10017';
+                    return true;
                 }
             }else{
-               die(Return_data(array('status_code' => '300', 'message' => sprintf('操作过于频繁，%s后再试！',date('i分s秒', $_SESSION['mon_yzm']['lasttime']-time())), 'data' => $_SESSION['mon_yzm']['ltime']-time() )));
+            	$this->_response_code = '10018';
+            	$this->_error[$this->_response_code]= sprintf('操作过于频繁，%s后再试！',date('i分s秒', $_SESSION[$this->_yzm_mobile]['lasttime']-time()));
+            	return false;
             }
         }else{
-            die(Return_data(array('status_code' => '300', 'message' => sprintf('请在%s秒后再次申请短信验证码',$_SESSION['mon_yzm']['ltime']-time()), 'data' => $_SESSION['mon_yzm']['ltime']-time() )));
+        	$this->_response_code = '10019';
+        	$this->_error[$this->_response_code]= sprintf('请在%s秒后再次申请短信验证码',$_SESSION[$this->_yzm_mobile]['ltime']-time());
+        	return false;
         }
-    	}
 	}
 
 }
