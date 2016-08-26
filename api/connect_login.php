37c5385ed382f1bffd795ebbf40acab1cecbb13b
zhanggaofei <zhgf@mayizaixian.cn>
2016-08-26 12:13:01 +0800

    up connect login

--- api/connect_login.php
+++ api/connect_login.php
@@ -25,66 +25,55 @@ class connect
 	private $_config = '';
 	private $_account = '';
 	private $_password = '';
+	private $_sina_akey = '';
+	private $_sina_skey = '';
+	private $_sina_callback_url = '';
+	private $_qq_akey = '';
+	private $_qq_skey = '';
 	private $_wx_akey = '';
 	private $_wx_skey = '';
-	private $_wx_callback_url = '';
 	private $_request = '';
 	private $_obj = '';
-	protected $_response_code = '';//回调状态
-	protected $_response_data = null;//回调数据
-	protected $_error = array(
-        '00000'=>'操作成功！',
-        '10001'=>'亲！玩我呢！',
-        '10002'=>'请求的方法不存在！'
-    );
+	private $_code = '';
 	
 	public function __construct()
 	{
 		global $connect_config,$config,$db,$buid;
-		$post = !empty($_REQUEST)?$_REQUEST:$this->_response_code='10001';
 
-
-
-		var_dump($buid);die;
 		$this->_config  = $config;
-		$this->_wx_akey = $connect_config['sina_app_id'];
-		$this->_wx_skey = $connect_config['sina_key'];
-		$this->_wx_callback_url = $config['weburl']."/api/connect_login.php?type=sina_connect";
+		$this->_sina_akey = $connect_config['sina_app_id'];
+		$this->_sina_skey = $connect_config['sina_key'];
+		$this->_sina_callback_url = $config['weburl']."/api/connect_login.php?action=sina_connect";
+
+		$this->_qq_akey = $connect_config['qq_app_id'];
+		$this->_qq_skey = $connect_config['qq_key'];
+
+		$this->_wx_akey = $connect_config['weixin_app_id'];
+    	$this->_wx_skey = $connect_config['weixin_key'];
+    	$this->_wx_callback_url = $config['weburl']."/api/connect_login.php?action=weixin_connect";
+		$this->_code = (isset($_REQUEST['code'])&&!empty($_REQUEST['code']))?$_REQUEST['code']:'';
 		
-		if(!empty($post['type'])){
-			$this->_action  = $post['type'];
-
-			if (method_exists($this,$this->_action)) {
-                call_user_func(array($this,$this->_action));
-            }else{
-                // 请求的方法不存在
-                $this->_response_code='10002';
-            }
-		}
-
-		$this->response(); 
 	}
 
-	private function response(){
-		var_dump(array('status'=>$this->_response_code,'errmsg'=>$this->_error[$this->_response_code],'data'=>$this->_response_data));die;
-		// echo json_encode(array('status'=>$this->_response_code,'errmsg'=>$this->_error[$this->_response_code],'url'=>$this->_response_data));die;
-
-	}
-
-
-	public function sina_connect(){
+	/**
+	 * [sina_connect description]
+	 * @param  [type] $type [description]
+	 * @return [type]       [description]
+	 */
+	public function sina_connect($type = null){
 		include_once( $this->_config['webroot'].'/includes/saetv2.ex.class.php' );
-		$this->_obj = new SaeTOAuthV2( $this->_wx_akey , $this->_wx_skey );
-		if(empty($this->_action))
-		return $this->_obj->getAuthorizeURL( $this->_wx_callback_url );
+		$this->_obj = new SaeTOAuthV2( $this->_sina_akey , $this->_sina_skey );
+		if($type == 'url')
+		return $this->_obj->getAuthorizeURL( $this->_sina_callback_url );
+		
 		//绑定会员
-	    if(!empty($this->_action)&&isset($_REQUEST['code']))
+	    if($this->_code)
 	    {
 	        $keys = array();
-	        $keys['code'] = $_REQUEST['code'];
-	        $keys['redirect_uri'] = $this->_wx_callback_url;
+	        $keys['code'] = $this->_code;
+	        $keys['redirect_uri'] = $this->_sina_callback_url;
 	        $token = $this->_obj->getAccessToken( 'code', $keys ) ;
-	        $c = new SaeTClientV2( $this->_wx_akey , $this->_wx_skey , $token['access_token'] );
+	        $c = new SaeTClientV2( $this->_sina_akey , $this->_sina_skey , $token['access_token'] );
 	        $uid_get = $c->get_uid();
 	        $uid = $uid_get['uid'];
 	        $ar = $c->show_user_by_id( $uid);
@@ -116,6 +105,129 @@ class connect
 	    //-------------------------------------------
 	}
 
+
+	/**
+	 * QQ关联登录
+	 */
+	public function qq_connect($type = null){
+
+		if($type == 'url')
+		return "https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=".$this->_qq_akey."&redirect_uri='".$this->_config['weburl']."/login.php'&state=".$this->_config['company']."&client_secret=".$this->_qq_skey;
+		$this->_config['return']=urlencode($this->_config['weburl'].'/login.php');
+	    $url="https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&"
+	        ."client_id=".$this->_qq_akey.""
+	        ."&client_secret=".$this->_qq_skey.""
+	        ."&code=".$this->_code.""
+	        ."&state=".$this->_config[company].""
+	        ."&redirect_uri=".$this->_config['return']."";
+	    $takenid=@get_url_contents($url);
+	    //----------------
+	    $url2="https://graph.qq.com/oauth2.0/me?$takenid";
+	    $con=get_url_contents($url2);
+	    $lpos = strpos($con, "(");
+	    $rpos = strrpos($con, ")");
+	    $con  = substr($con, $lpos + 1, $rpos - $lpos -1);
+	    $ar2=json_decode($con,true);
+	    //----------------
+	    $url3 = "https://graph.qq.com/user/get_user_info?"
+	        . $takenid
+	        . "&oauth_consumer_key=" . $this->_qq_akey
+	        . "&openid=" . $ar2["openid"]
+	        . "&format=json";
+
+	    $con=get_url_contents($url3);
+	    $ar=json_decode($con,true);
+	    //--------------------------
+
+	    $sql="select * from ".USERCOON." where type=1 and openid='$ar2[openid]'";
+	    $db->query($sql);
+	    $cre=$db->fetchRow();
+	    if(empty($cre['id']))
+	    {
+	        $sql="insert into ".USERCOON."
+	        (nickname,figureurl,gender,vip,level,type,access_token,client_id,openid) 
+	        values 
+	        ('$ar[nickname]','$ar[figureurl]','$ar[gender]','$ar[vip]','$ar[level]',1,'$takenid','$ar2[client_id]','$ar2[openid]')";
+	        $db->query($sql);
+	        $cre['id']=$db->lastid();
+	    }
+	    if($cre['userid'])
+	    {
+	        login($cre['userid'],NULL);
+	        $forward = $post['forward']?$post['forward']:$config["weburl"]."/main.php?cg_u_type=1";
+	        msg($forward);
+	    }
+	    else
+	    {
+	        msg("login.php?connect_id=$cre[id]&connect_nickname=" . urlencode($ar['nickname']));
+	    }
+
+
+	}
+
+	/**
+	 * 微信关联登录
+	 */
+	public function weixin_connect($type = null){
+			if($type == 'url'){
+				return $this->weixin_code();
+			}
+			if(!isset($_SESSION['openid_connect']) || $_SESSION['openid_connect']==""){
+				//获取code
+				if(empty($this->_code))$this->weixin_code();
+			    $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$this->_wx_akey.'&secret='.$this->_wx_skey.'&code='.$this->_code.'&grant_type=authorization_code';
+			    $token = json_decode(file_get_contents($token_url));
+
+			    $access_token_url = 'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid='.$appid.'&grant_type=refresh_token&refresh_token='.$token->refresh_token;
+		        $access_token = json_decode(file_get_contents($access_token_url));
+
+		        $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token->access_token.'&openid='.$access_token->openid.'&lang=zh_CN';
+		        $user_info = json_decode(@file_get_contents($user_info_url));
+
+		        $_SESSION['openid_connect'] = $openid = $user_info -> openid;
+		        $nickname = $user_info -> nickname;
+			}
+			if(!empty($_SESSION['openid_connect'])){
+				$sql = "select * from ".USERCOON." where type=3 and openid='".$_SESSION['openid_connect']."'";
+				$this->_db->query($sql);
+				$cre = $this->_db->fetchRow();
+				if(empty($cre['id']))
+	            {
+	                $sql="insert into ".USERCOON."(nickname,figureurl,gender,vip,level,type,access_token,client_id,openid)
+	                        values('$nickname','$ar[figureurl]','$ar[gender]','$ar[vip]','$ar[level]',3,'$takenid','$ar2[client_id]','$openid')";
+	                $db->query($sql);
+	                $cre['id']=$db->lastid();
+	                if(!empty($cre['userid']))
+		            {
+		                login($cre['userid'],NULL);
+		                $forward = $config["weburl"]."/main.php?cg_u_type=1";
+		                msg($forward);
+		            }
+		            else
+		            {
+		                $_SESSION['connect_name'] = '微信';
+		                msg("login.php?connect_id=$cre[id]");
+		            }
+	            }
+			}
+	}
+
+	/**
+	 * 微信互联登录获取code
+	 */
+	private function weixin_code(){
+		if($config['bw'] == "weixin"){
+		    $weixin_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$this->_wx_akey."&redirect_uri=".$this->_wx_callback_url."&response_type=code&scope=snsapi_login&state=123&connect_redirect=1#wechat_redirect";
+		    header('location:' . $weixin_url);
+		    return true;
+		}else
+	        return "https://open.weixin.qq.com/connect/qrconnect?appid=".$this->_wx_akey."&redirect_uri=".$this->_wx_callback_url."&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect";
+	}
+
+	/**
+	 * [users description]
+	 * @return [type] [description]
+	 */
 	private function users(){
 		$sql = "select userid,user,statu,pid,password,mobile,rand_pwd from ".MEMBER." where mobile={$this->_account}";
 		$this->_db->query($sql);
@@ -140,7 +252,27 @@ class connect
 
 
 }
-
+$post = !empty($_REQUEST['action'])?$_REQUEST['action']:'';
 $obj = new connect();
+$config['_SINA_URL']= $obj->sina_connect('url');
+$config['_QQ_URL'] = $obj->qq_connect('url');
+$config['_WX_URL'] = $obj->weixin_connect('url');
+
+switch ($post) {
+	case 'sina_connect':
+		$obj->sina_connect();
+		break;
+	case 'qq_connect':
+		$obj->qq_connect();
+		break;
+	case 'weixin_connect':
+		$obj->weixin_connect();
+		break;
+		
+	default:
+		# code...
+		break;
+}
+
 
 ?>
