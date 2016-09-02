68bbba33bd355301ac001e315404f9813fb8f5fb
huwei <2570328000@qq.com>
2016-09-02 14:42:33 +0800

    aaaa

--- script/wap/page/hj_login.js
+++ script/wap/page/hj_login.js
@@ -64,7 +64,8 @@ define(["module", "utility",  "formValid"], function(module, Util, formValid) {
                 $(this).parent().find(obj2).removeClass(obj3);
             });  
         });
-        $(obj2).on("tap", function(){ 
+        $(obj2).on("tap", function(e){
+            utility.stopPropagation(e); 
             $(this).parent().find("input").val("");   
         });  
     }
